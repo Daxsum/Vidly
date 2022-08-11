308659a16d0784cf7a0384b4d7af8e4042c1fa0e
Daxsum <kaldax250@gmail.com>
2022-08-11 19:55:48 +0300

    first commit

---
+++ src/components/comm/like.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const Like = (props) => {
+    let Class="fa-solid fa-heart"; 
+    if  (!props.Liked){Class="fa-regular fa-heart"}
+
+    return (
+
+
+<i 
+onClick={props.onLiked}
+className={Class}></i>
+    );
+}
+ 
+export default Like;
+
+ 
